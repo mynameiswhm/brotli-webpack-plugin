3cbaf07eff4b05636ad8448aafe833ea32fe7c1a
Ilia Saulenko <i.saulenko@office.ngs.ru>
2016-07-29 17:43:53 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# brotli plugin for webpack
+
+This plugin compresses assets with [Brotli](https://github.com/google/brotli) compression algorithm using [iltorb](https://github.com/MayhemYDG/iltorb#brotliparams) library for serving it with [ngx_brotli](https://github.com/google/ngx_brotli) or such. 
+
+## Usage
+
+``` javascript
+var BrotliPlugin = require('brotli-webpack-plugin');
+module.exports = {
+	plugins: [
+		new CompressionPlugin({
+			asset: '[path].br[query]',
+			test: /\.(js|css|html|svg)$/,
+			threshold: 10240,
+			minRatio: 0.8
+		})
+	]
+}
+```
+
+Arguments:
+
+* `asset`: The target asset name. `[file]` is replaced with the original asset file name. `[path]` is replaced with the path of the original asset and `[query]` with the query. Defaults to `'[path].br[query]'`.
+* `test`: All assets matching this RegExp are processed. Defaults to every asset.
+* `threshold`: Only assets bigger than this size (in bytes) are processed. Defaults to `0`.
+* `minRatio`: Only assets that compress better that this ratio are processed. Defaults to `0.8`.
+
+Optional arguments for Brotli (see [iltorb](https://github.com/MayhemYDG/iltorb#brotliparams) doc for details):
+* `mode`: Default: 0,
+* `quality`: Default: 11,
+* `lgwin`: Default: 22,
+* `lgblock`: Default: 0,
+* `enable_dictionary`: Default: true,
+* `enable_transforms`: Default: false,
+* `greedy_block_split`: Default: false,
+* `enable_context_modeling`: Default: false
+
+## License
+
+Heavily copy-pasted from [webpack/compression-webpack-plugin](https://github.com/webpack/compression-webpack-plugin) by [Tobias Koppers](https://github.com/sokra).
+
+Licensed under [MIT](./LICENSE).
---
+++ index.js
@@ -0,0 +1,81 @@
+var async = require('async');
+var url = require('url');
+
+var RawSource = require('webpack-sources/lib/RawSource');
+
+function CompressionPlugin(options) {
+    options = options || {};
+    this.asset = options.asset || '[path].gz[query]';
+    this.test = options.test || options.regExp;
+    this.threshold = options.threshold || 0;
+    this.minRatio = options.minRatio || 0.8;
+
+    try {
+        var brotli = require('iltorb');
+    } catch (err) {
+        throw new Error('iltorb not found');
+    }
+
+    var brotliOptions = {
+        mode: options.hasOwnProperty('mode') ? options.mode : 0,
+        quality: options.hasOwnProperty('quality') ? options.quality : 11,
+        lgwin: options.hasOwnProperty('lgwin') ? options.lgwin : 22,
+        lgblock: options.hasOwnProperty('lgblock') ? options.lgblock : 0,
+        enable_dictionary: options.hasOwnProperty('enable_dictionary') ? options.enable_dictionary : true,
+        enable_transforms: options.hasOwnProperty('enable_transforms') ? options.enable_transforms : false,
+        greedy_block_split: options.hasOwnProperty('greedy_block_split') ? options.greedy_block_split : false,
+        enable_context_modeling: options.hasOwnProperty('enable_context_modeling') ? options.enable_context_modeling : false
+    };
+
+    this.compress = function (content, callback) {
+        brotli.compress(content, brotliOptions, callback);
+    };
+}
+module.exports = CompressionPlugin;
+
+CompressionPlugin.prototype.apply = function (compiler) {
+    compiler.plugin('this-compilation', function (compilation) {
+        compilation.plugin('optimize-assets', function (assets, callback) {
+            async.forEach(Object.keys(assets), function (file, callback) {
+                if (this.test && !this.test.test(file)) {
+                    return callback();
+                }
+
+                var asset = assets[file];
+                var content = asset.source();
+                if (!Buffer.isBuffer(content)) {
+                    content = new Buffer(content, 'utf-8');
+                }
+
+                var originalSize = content.length;
+                if (originalSize < this.threshold) {
+                    return callback();
+                }
+
+                this.compress(content, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    if (result.length / originalSize > this.minRatio) {
+                        return callback();
+                    }
+
+                    var parse = url.parse(file);
+                    var sub = {
+                        file: file,
+                        path: parse.pathname,
+                        query: parse.query || ''
+                    };
+
+                    var newFile = this.asset.replace(/\[(file|path|query)\]/g, function (p0, p1) {
+                        return sub[p1];
+                    });
+
+                    assets[newFile] = new RawSource(result);
+                    callback();
+                }.bind(this));
+            }.bind(this), callback);
+        }.bind(this));
+    }.bind(this));
+};
